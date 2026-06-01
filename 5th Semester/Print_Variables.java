6e10e3ebd90008e66e9e4fd7f8e09bf5e1d6bea8
Mahinur Rahaman Mahin <taslima2422@gmail.com>
2026-06-01 13:25:58 +0600

    Add files via upload

---
+++ 5th Semester/Print_Variables.java
@@ -0,0 +1,18 @@
+public class Print_Variables 
+{
+    public static void main (String[] args)
+    {
+        String firstName = "John " ;
+
+        String lastName = "Doe" ;
+
+        String fullName = firstName + lastName ;
+
+        System.out.println(fullName) ;
+    
+    
+    }                    
+
+    
+
+}
