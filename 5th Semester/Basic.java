0a92983eaab2647ca214cd6ce7465541b2162599
Mahinur Rahaman Mahin <taslima2422@gmail.com>
2026-06-03 11:45:48 +0600

    Add files via upload

---
+++ 5th Semester/Basic.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.basic;
+
+public class Basic 
+        
+{
+
+    public static void main(String[] args) 
+            
+    {
+        System.out.println("Hello World!") ;
+    }
+    
+}
