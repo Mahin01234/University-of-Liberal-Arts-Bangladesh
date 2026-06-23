2d2585a826a20d4c2c044f49fe7fda629bc3a4d2
Mahinur Rahaman Mahin <taslima2422@gmail.com>
2026-06-23 21:32:43 +0600

    Add files via upload

--- 5th Semester/Student.java
+++ 5th Semester/Student.java
@@ -1,243 +1,52 @@
 
 
-import java.util.Scanner;
+package com.mycompany.student;
+
+/**
+ *
+ * @author DELL
+ */
+
 
 public class Student 
-
-
 {
 
-    // Object Variables
-    String id;
-    String name;
-    String department;
-    double cgpa;
+    private String name;
+    private String id;
+    private double cgpa;
+    private String department;
 
-
-
-
-
-    // Class Variables
-    static String university = "University of Liberal Arts Bangladesh (ULAB)";
-    static int totalStudents = 0;
-
-
-
-
-    // Parameterized Constructor
-    Student(String id, String name, String department, double cgpa) 
-    
-    
+    // Constructor 1
+    public Student(String name, String id, double cgpa, String department) 
     {
-
-        this.id = id;
         this.name = name;
-        this.department = department;
+        this.id = id;
         this.cgpa = cgpa;
-
-        totalStudents++;
+        this.department = department;
     }
 
-
-
-
-
-    // Object Method
-    void displayStudentDetails() 
-    
-    
-    
+    // Constructor 2
+    public Student(String name, String id) 
     {
-
-        System.out.println("\nStudent Details");
-        System.out.println("-------------------------");
-        System.out.println("ID : " + id);
-        System.out.println("Name : " + name);
-        System.out.println("Department : " + department);
-        System.out.println("CGPA : " + cgpa);
-        System.out.println("University : " + university);
+        this.name = name;
+        this.id = id;
+        this.cgpa = 0.0;
+        this.department = "Not Specified";
     }
 
-
-
-
-
-
-    // Class Method
-    static void displayTotalStudents() 
-    
-    
-    
+    // Overloaded Method 1
+    public void show() 
     {
-
-        System.out.println("\nTotal Number of Students : " + totalStudents);
+        System.out.println("Name: " + name);
+        System.out.println("ID: " + id);
+        System.out.println("CGPA: " + cgpa);
+        System.out.println("Department: " + department);
     }
 
-
-
-
-
-    public static void main(String[] args) 
-    
+    // Overloaded Method 2
+    public void show(String title) 
     {
-
-
-
-        Scanner s1 = new Scanner(System.in);
-
-
-
-
-        // Student 1 Input
-        System.out.println("Enter the 1st Student Information");
-
-
-
-
-        System.out.print("ID : ");
-        String id1 = s1.nextLine();
-
-
-
-
-        System.out.print("Name : ");
-        String name1 = s1.nextLine();
-
-
-
-
-        System.out.print("Department : ");
-        String department1 = s1.nextLine();
-
-
-
-
-
-        System.out.print("CGPA : ");
-        double cgpa1 = s1.nextDouble();
-        s1.nextLine();
-
-
-
-
-
-        Student student1 = new Student(id1, name1, department1, cgpa1);
-
-
-
-
-
-        // Student 2 Input
-        System.out.println("\nEnter the 2nd Student Information");
-
-
-
-
-
-        System.out.print("ID : ");
-        String id2 = s1.nextLine();
-
-
-
-
-        System.out.print("Name : ");
-        String name2 = s1.nextLine();
-
-
-
-
-
-        System.out.print("Department : ");
-        String department2 = s1.nextLine();
-
-
-
-
-
-        System.out.print("CGPA : ");
-        double cgpa2 = s1.nextDouble();
-        s1.nextLine();
-
-
-
-
-
-        Student student2 = new Student(id2, name2, department2, cgpa2);
-
-
-
-
-
-        // Student 3 Input
-        System.out.println("\nEnter the 3rd Student Information");
-
-
-
-
-
-        System.out.print("ID : ");
-        String id3 = s1.nextLine();
-
-
-
-
-
-        System.out.print("Name : ");
-        String name3 = s1.nextLine();
-
-
-
-
-
-        System.out.print("Department : ");
-        String department3 = s1.nextLine();
-
-
-
-
-
-        System.out.print("CGPA : ");
-        double cgpa3 = s1.nextDouble();
-
-
-
-
-
-        Student student3 = new Student(id3, name3, department3, cgpa3);
-
-
-
-
-
-        // Display Student Details
-        System.out.println("\n===== Student 1 Information =====");
-        student1.displayStudentDetails();
-
-
-
-
-
-
-        System.out.println("\n===== Student 2 Information =====");
-        student2.displayStudentDetails();
-
-
-
-
-
-        System.out.println("\n===== Student 3 Information =====");
-        student3.displayStudentDetails();
-
-
-
-
-        // Display Total Students
-        Student.displayTotalStudents();
-
-
-
-
-        
-        s1.close();
+        System.out.println(title);
+        show();
     }
 }
